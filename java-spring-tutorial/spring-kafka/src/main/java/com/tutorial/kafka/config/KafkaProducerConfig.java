61bc4f28e50ce88d3a2418a0af24826127e4c07c
uonyekwuluje <uionyekwuluje@gmail.com>
2022-04-20 12:32:31 -0400

    new code

---
+++ java-spring-tutorial/spring-kafka/src/main/java/com/tutorial/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package com.tutorial.kafka.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.config.TopicBuilder;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    // Create Map
+    public Map<String, Object> producerConfig() {
+      
+    }    
+
+}
--- java-spring-tutorial/spring-kafka/src/main/java/com/tutorial/kafka/config/KafkaTopicConfig.java
+++ java-spring-tutorial/spring-kafka/src/main/java/com/tutorial/kafka/config/KafkaTopicConfig.java
@@ -1,7 +1,4 @@
-package com.tutorial.kafka.topic;
-
-import java.util.HashMap;
-import java.util.Map;
+package com.tutorial.kafka.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -16,20 +13,35 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @Configuration
+@SpringBootApplication
+@RestController
 public class KafkaTopicConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
     
-    @Bean 
-    public KafkaAdmin admin() {
-       return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress));
+    //@Bean 
+    //public KafkaAdmin admin() {
+    //   return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress));
+    //}
+
+    @GetMapping("/topic")
+    public String hello(@RequestParam(value = "name", defaultValue = "spapptpc") String name) {
+       return String.format("Hello %s!", name);
     }
 
     // Create Topic
+    // NewTopic from kafka admin clients package
     @Bean
     public NewTopic topicIntro() {
         final NewTopic topic = TopicBuilder.name("sapp")
@@ -39,8 +51,5 @@ public class KafkaTopicConfig {
 
         return topic;
     }
-
-    // List Topics
-
 
 }
