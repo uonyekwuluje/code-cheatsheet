976d01cb948355f56c09daf85028e34ec9b0b1dd
uonyekwuluje <uionyekwuluje@gmail.com>
2022-04-20 16:18:35 -0400

    kaka controllrt API

--- java-spring-tutorial/spring-kafka/README.md
+++ java-spring-tutorial/spring-kafka/README.md
@@ -1,9 +1,18 @@
 # Compile Test 
 ```
-mvn install
+mvn clean package
 ```
 
 # Test App
 ```
 ./mvnw spring-boot:run
 ```
+Test with POST with payload
+```
+curl -X POST http://localhost:8080/api/v1/messages \
+-H 'Content-Type: application/json' \
+-d '{"message":"CML Kafka Test"}'
+```
+
+# Reference
+* [Spring for Apache Kafka](https://spring.io/projects/spring-kafka)
--- java-spring-tutorial/spring-kafka/src/main/java/com/tutorial/kafka/config/KafkaProducerConfig.java
+++ java-spring-tutorial/spring-kafka/src/main/java/com/tutorial/kafka/config/KafkaProducerConfig.java
@@ -1,11 +1,20 @@
 package com.tutorial.kafka.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -29,7 +38,22 @@ public class KafkaProducerConfig {
 
     // Create Map
     public Map<String, Object> producerConfig() {
-      
+      Map<String, Object> props = new HashMap<>();
+      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
+      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+      return props;
     }    
 
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    } 
+
+    @Bean 
+    public KafkaTemplate<String, String> kafkaTemplate(
+        ProducerFactory<String, String> producerFactory
+    ) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
