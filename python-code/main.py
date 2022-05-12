3e91520d671a3ad0f5c41755528a0ddaa3d69649
uonyekwuluje <uionyekwuluje@gmail.com>
2022-05-12 03:54:05 -0400

    test ground

--- python-code/main.py
+++ python-code/main.py
@@ -5,4 +5,13 @@ from basics import p_list
 
 if __name__ == '__main__':
     p_list.listOps()
-   
+    nstr = "Mattapan"
+    random_string = "This is a random string"
+    G = 6.67 * (10 ** -11)
+    M = 6.0 * (10 ** 24)  # Mass of Earth
+    m = 7.34 * (10 ** 22)  # Mass of the moon
+    r = 3.84 * (10 ** 8)
+
+    # Write your code here
+    grav_force = (G * M * m)/ r ** 2
+    print(grav_force)
